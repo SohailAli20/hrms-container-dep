d8125b187dc0ef64d4d14c26fd0e745a3534844f
Shiva Kumar Karinigi <shiva.karinigi@synectiks.com>
2024-02-17 17:26:30 +0530

    feat:made minor changes

--- employee/getEmployee.js
+++ employee/getEmployee.js
@@ -1,7 +1,7 @@
 const { connectToDatabase } = require("../db/dbConnector");
 const { z } = require("zod");
 exports.handler = async (event) => {
-    const employeeId = event.queryStringParameters.employee_id;
+    const employeeId =event.queryStringParameters?.employee_id ?? null;
     const idSchema = z.string().uuid({ message: "Invalid employee id" });
     const isUuid = idSchema.safeParse(employeeId);
     if (!isUuid.success) {
@@ -15,8 +15,8 @@ exports.handler = async (event) => {
             }),
         };
     }
-    const client = await connectToDatabase();
 
+    const client = await connectToDatabase();
     try {
         const query = `
             SELECT 
@@ -59,50 +59,58 @@ exports.handler = async (event) => {
 
         const result = await client.query(query, [employeeId]);
         const formattedResult = result.rows.map((row) => ({
-            emp_id: row.emp_id,
-            email: row.email,
-            password: row.password,
-            work_email: row.work_email,
-            first_name: row.first_name,
-            last_name: row.last_name,
-            gender: row.gender,
-            dob: row.dob,
-            number: row.number,
-            emergency_number: row.emergency_number,
-            highest_qualification: row.highest_qualification,
-            address_id: row.address_id,
-            emp_detail_id: row.emp_detail_id,
-            address_line_1: row.address_line_1,
-            address_line_2: row.address_line_2,
-            landmark: row.landmark,
-            country: row.country,
-            state: row.state,
-            city: row.city,
-            zipcode: row.zipcode,
-            designation_id: row.designation_id,
-            pf: row.pf,
-            uan: row.uan,
-            department_id: row.department_id,
-            reporting_manager_id: row.reporting_manager_id,
-            emp_type_id: row.emp_type_id,
-            work_location: row.work_location,
-            start_date: row.start_date,
-            department_name: row.department_name,
-            emp_type: row.emp_type,
-            emp_designation_id: row.emp_designation_id,
-            emp_designation: row.emp_designation,
-            reporting_manager_first_name: row.reporting_manager_first_name,
-            reporting_manager_last_name: row.reporting_manager_last_name,
-            document_id: row.document_id,
-            name: row.name,
-            url: row.url,
-            owner: row.owner,
-            device_type_id: row.device_type_id,
-            manufacturer: row.manufacturer,
-            serial_number: row.serial_number,
-            note: row.note,
-            supply_date: row.supply_date,
-            device_type_name: row.device_type_name,
+            personal_information: {
+                emp_id: row.emp_id,
+                email: row.email,
+                password: row.password,
+                work_email: row.work_email,
+                first_name: row.first_name,
+                last_name: row.last_name,
+                gender: row.gender,
+                dob: row.dob,
+                number: row.number,
+                emergency_number: row.emergency_number,
+                highest_qualification: row.highest_qualification,
+                address_id: row.address_id,
+                address_line_1: row.address_line_1,
+                address_line_2: row.address_line_2,
+                landmark: row.landmark,
+                country: row.country,
+                state: row.state,
+                city: row.city,
+                zipcode: row.zipcode,
+            },
+            professional_information: {
+                emp_detail_id: row.emp_detail_id,
+                designation_id: row.designation_id,
+                pf: row.pf,
+                uan: row.uan,
+                department_id: row.department_id,
+                reporting_manager_id: row.reporting_manager_id,
+                emp_type_id: row.emp_type_id,
+                work_location: row.work_location,
+                start_date: row.start_date,
+                department_name: row.department_name,
+                emp_type: row.emp_type,
+                emp_designation_id: row.emp_designation_id,
+                emp_designation: row.emp_designation,
+                reporting_manager_first_name: row.reporting_manager_first_name,
+                reporting_manager_last_name: row.reporting_manager_last_name,
+            },
+            documents: {
+                document_id: row.document_id,
+                name: row.name,
+                url: row.url,
+            },
+            equipment: {
+                owner: row.owner,
+                device_type_id: row.device_type_id,
+                manufacturer: row.manufacturer,
+                serial_number: row.serial_number,
+                note: row.note,
+                supply_date: row.supply_date,
+                device_type_name: row.device_type_name,
+            },
         }));
 
         return {
