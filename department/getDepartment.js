91b2adc9427033f76615e7449d87201c1b5f498b
BalineniAmarnath <amarnath.balineni@synectiks.com>
2024-02-19 15:30:43 +0530

    modified two apis

--- department/getDepartment.js
+++ department/getDepartment.js
@@ -1,23 +1,12 @@
 const { connectToDatabase } = require("../db/dbConnector");
-const { z } = require("zod");
 exports.handler = async (event) => {  
-    const FiltersSchema = z.object({
-        org_id: z.string().uuid(), 
-    });
     const client = await connectToDatabase();
     try {
-        const { filters } = event.queryStringParameters;
-        let parsedFilters = {};
-        if (filters) {
-            parsedFilters = FiltersSchema.parse(JSON.parse(filters));
-        }
-        let query = `SELECT * FROM department`;
-        if (Object.keys(parsedFilters).length > 0) {
-            const filterClauses = Object.keys(parsedFilters).map((key) => `${key} = '${parsedFilters[key]}'`);
-            query += ` WHERE ${filterClauses.join(' AND ')}`;
-        }
+        const query = 'SELECT * FROM department';
         const result = await client.query(query);
+        
         const departments = result.rows;
+        
         return {
             statusCode: 200,
             headers: {
@@ -32,9 +21,11 @@ exports.handler = async (event) => {
                 "Access-Control-Allow-Origin": "*",
             },
             body: JSON.stringify({
-                message: "Internal Server Error",
-                error: error.message,
+                message: error.message,
+                error: error,
             }),
         };
+    } finally {
+        await client.end();
     }
 };
