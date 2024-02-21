923fa77f0c3e94240b5d4fbc0bf9db28abd7ae5b
SohailAli20 <mohammad.ali@synectiks.com>
2024-02-21 14:32:08 +0530

    corrected the pagination query logic

--- employee/getEmployees.js
+++ employee/getEmployees.js
@@ -2,47 +2,31 @@ const { connectToDatabase } = require("../db/dbConnector");
 const { z } = require("zod");
 
 exports.handler = async (event) => {
-    const queryParametersSchema = z.object({
-        page: z.string().regex(/^\d+$/),
-        pageSize: z.string().regex(/^\d+$/),
-    });
-
-    const { page = 1, pageSize = 10 } = queryParametersSchema.parse(event.queryStringParameters);
-
-    const offset = (page - 1) * pageSize;
-
+    const page = event.queryStringParameters?.page ?? null
+    let offset = (page - 1) * 10;
+    offset = Math.max(offset, 0);
     const client = await connectToDatabase();
-
+    console.log(offset)
     try {
         const query = `
-            SELECT
-                e.id,
-                e.first_name,
-                e.last_name,
-                e.work_email,
-                ed.designation,
-                et.type AS emp_type,
-                d.name AS department,
-                ed2.start_date
-            FROM
-                employee e
-            LEFT JOIN emp_designation ed ON e.org_id::uuid = ed.org_id::uuid
-            LEFT JOIN emp_type et ON e.org_id::uuid = et.org_id::uuid
-            LEFT JOIN department d ON e.org_id::uuid = d.org_id::uuid
-            LEFT JOIN emp_detail ed2 ON e.emp_detail_id::uuid = ed2.id::uuid
-            WHERE
-                e.first_name IS NOT NULL AND
-                e.last_name IS NOT NULL AND
-                e.id IS NOT NULL AND
-                e.work_email IS NOT NULL AND
-                ed.designation IS NOT NULL AND
-                et.type IS NOT NULL AND
-                d.name IS NOT NULL AND
-                ed2.start_date IS NOT NULL
-            GROUP BY
-                e.id, e.first_name, e.last_name, e.work_email, ed.designation, et.type, d.name, ed2.start_date
-            ORDER BY e.id
-            LIMIT ${pageSize} OFFSET ${offset}
+                SELECT
+                    e.id,
+                    e.first_name,
+                    e.last_name,
+                    e.work_email,
+                    ed.designation,
+                    ed2.employee_id,
+                    et.type AS emp_type,
+                    d.name AS department,
+                    ed2.start_date
+                FROM
+                    employee e
+                LEFT JOIN emp_detail ed2 ON e.emp_detail_id = ed2.id
+                LEFT JOIN emp_designation ed ON ed2.designation_id = ed.id
+                LEFT JOIN emp_type et ON ed2.emp_type_id = et.id
+                LEFT JOIN department d ON ed2.department_id = d.id
+                ORDER BY e.first_name 
+                LIMIT 10 OFFSET ${offset}
         `;
 
         const EmployeeMetaData = await client.query(query);
