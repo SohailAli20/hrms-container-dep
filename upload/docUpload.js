214a0d9efdd70f4e3973eadf25a0a7b4ca3dc7cf
sukantahalder123 <128042790+sukantahalder123@users.noreply.github.com>
2024-02-27 11:32:39 +0530

    Update docUpload.js

--- upload/docUpload.js
+++ upload/docUpload.js
@@ -6,32 +6,15 @@ const FOLDER = 'workflow_DMS/';
 
 exports.handler = async (event) => {
   try {
-    const contentTypeHeader = event.headers['Content-Type'] || event.headers['content-type'];
-    console.log('Content-Type Header:', contentTypeHeader);
-
-    if (!contentTypeHeader || !contentTypeHeader.includes('multipart/form-data')) {
-      throw new Error('Invalid or missing Content-Type header. Expected "multipart/form-data"');
-    }
-
+    const contentTypeHeader = event.headers['Content-Type'];
     const bodyBuffer = Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'binary');
     const boundary = multipart.getBoundary(contentTypeHeader);
     const parts = multipart.Parse(bodyBuffer, boundary);
-
-    if (!parts || parts.length === 0) {
-      throw new Error('No valid parts found in the request body.');
-    }
     const filePart = parts.find(part => part.filename);
-    
-    if (!filePart) {
-      throw new Error('No file part found in the request body.');
-    }
-
-    const { filename, data, type } = filePart;
-    console.log('File Details:', { filename, type });
+    const { data, type } = filePart;
     const imageFormat = type.split('/')[1] || 'unknown';
     const fileName = `formData_${Date.now()}.${imageFormat}`;
     const decodedImageData = Buffer.from(data, 'base64');
-
     const s3params = {
       Bucket: BUCKET,
       Key: `${FOLDER}${fileName}`,
@@ -39,16 +22,16 @@ exports.handler = async (event) => {
       ContentType: type,
     };
     await s3.upload(s3params).promise();
+    const link = `https://${BUCKET}.s3.amazonaws.com/${FOLDER}${fileName}`;
     return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${FOLDER}${fileName}` }),
+      body: JSON.stringify({ link }),
     };
   } catch (err) {
     console.error('Error:', err.message);
-
     return {
       statusCode: 500,
       headers: {
