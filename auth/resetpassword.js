009e2b54722da7ecbe41886191f0cc71e2ab3d0c
Chetan Meegada <chetan.meegada@synectiks.com>
2024-02-28 11:22:09 +0530

    feat: validation using zod

--- auth/resetpassword.js
+++ auth/resetpassword.js
@@ -1,18 +1,37 @@
 require("dotenv").config();
+const { z } = require("zod");
 const { CognitoIdentityProviderClient, ConfirmForgotPasswordCommand } = require("@aws-sdk/client-cognito-identity-provider");
 
 exports.handler = async (event, context) => {
     const requestBody = JSON.parse(event.body);
-    const username = requestBody.email;
-    const confirmationCode = requestBody.otp.trim();
-    const newPassword = requestBody.newPassword;
-
+    const req = {
+        email: requestBody.email,
+        confirmationCode:requestBody.otp.trim(),
+        newPassword:requestBody.newPassword
+    };
+    const reqSchema = z.object({
+        email: z.string().email(),
+        confirmationCode: z.string(),
+        newPassword: z.string()
+    });
+    const valResult = reqSchema.safeParse(req);
+    if (!valResult.success) {
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                error: valResult.error.formErrors.fieldErrors,
+            }),
+        };
+    }
     const client = new CognitoIdentityProviderClient({ region: "us-east-1" }); 
     const input = {
         ClientId: process.env.COGNITO_CLIENT_ID,
-        Username: username,
-        ConfirmationCode: confirmationCode,
-        Password: newPassword,
+        Username: req.email,
+        ConfirmationCode: req.confirmationCode,
+        Password: req.newPassword,
     };
 
     try {
